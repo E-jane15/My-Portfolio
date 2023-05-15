1588c9949cbc673e2f68763c7c45a1af92f06514
E-jane15 <janeebaimay@gmail.com>
2023-05-15 13:33:34 +0100

    Ebai Portfolio

---
+++ myportfolio/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+ReactDOM.render(<App/> , document.getElementById('root')); //THIS LINE INDICATES THAT 
+//ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE.
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
