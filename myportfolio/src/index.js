e8c489d39e47f9113982612a0591e875126f6cfa
E-jane15 <janeebaimay@gmail.com>
2023-05-16 22:43:36 +0100

    Ebai Portfolio

--- myportfolio/src/index.js
+++ myportfolio/src/index.js
@@ -5,7 +5,6 @@ ReactDOM.render(<App/> , document.getElementById('root')); //THIS LINE INDICATES
 //ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE.
 
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
