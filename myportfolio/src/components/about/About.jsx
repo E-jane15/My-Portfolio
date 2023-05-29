ef5589175f40b387c092479a4e53bcd2a060fee2
E-jane15 <janeebaimay@gmail.com>
2023-05-29 20:38:42 +0100

    Experience.experience__container {

--- myportfolio/src/components/about/About.jsx
+++ myportfolio/src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me.jpg'
+import {FiAward} from 'react-icons/fi'
+import {BiUserPlus} from 'react-icons/bi'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about '>
+      <h5>Get To Know</h5>
+      <h2> Me More</h2>
+      <div className="container about__container">
+       <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About image" />
+          </div>
+        </div>
+        <div className="about__content">
+        <div className='about__cards'>
+          <article className='about__card'>
+            <FiAward className='about-icon'/> 
+            <h5> Experience</h5>
+            <small>1 year experience </small>
+          </article>
+           
+          <article className='about__card'>
+            <BiUserPlus className='about-icon'/> 
+            <h5> Clients</h5>
+            <small>30 nationwide </small>
+          </article>
+
+          <article className='about__card'>
+            <AiOutlineFolderOpen className='about-icon'/> 
+            <h5> Projects</h5>
+            <small> 18+ finished</small>
+          </article>
+        </div>
+
+        <p>
+          I look forward to handling all of your worries. Client Satisfaction is my top priority
+        </p>
+        <a href="#contact" className='btn btn-primary'>
+           Let's link up
+        </a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- myportfolio/src/components/navbar/Navbar.jsx
+++ myportfolio/src/components/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './navbar.css'
-
+import {FiHome} from 'react-icons/fi'
+import {BiUserCircle} from 'react-icons/bi'
+import {BsBookmark} from 'react-icons/bs'
+import {RiServiceLine} from 'react-icons/ri'
+import {MdPermContactCalendar} from 'react-icons/md'
+import {useState} from 'react'
 const Navbar = () => {
+  const [activeNav,setActiveNav] = useState('#')
   return (
-    <div>Navbar</div>
+    <nav>
+       <a href="#" 
+      onClick={() => setActiveNav('#')}   className={activeNav=== '#' ? 'active' : ''}><FiHome/></a>
+       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle/></a>
+       <a href="#experience"  onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBookmark/></a>
+       <a href="#services"  onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+       <a href="#contact"  onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdPermContactCalendar/></a>
+    </nav>
   )
 }
 
